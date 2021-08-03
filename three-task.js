7dad154e078b71b85f3e8a426f9a64f308ad568b
Анастасия Воронкова <voronkova__ab@mail.ru>
2021-08-03 15:18:33 +0500

    Заменила разделитель строки на регулярное выражение

--- three-task.js
+++ three-task.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const waysStr = fs.readFileSync('text3.txt').toString();
-const waysArr = waysStr.split('\r\n');
+const waysArr = waysStr.split(/\r\n/);
 
 waysArr.forEach(way => {
     if (fs.existsSync(way)) {
